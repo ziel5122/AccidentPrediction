b9dd2ccbb52807a6fb15a7b08646362d92a90de1
ziel5122 <azielinski@csumb.edu>
2016-10-06 04:15:12 -0700

    pushing latest csv

--- libraries/cross-validate.R
+++ libraries/cross-validate.R
@@ -3,14 +3,26 @@
 # Supports e1071, rpart, and RandomForest
 #
 
-crossValidate.rpart = function(data, formula) {
+crossValidate.rpart = function(data, features, target, folds=5) {
+    n = nrow(data)
+    formula = reformulate(features, target)
     
+    data.sets = splitData(accident.GES, 5)
+    average = 0
+    
+    for (i in i:folds) {
+        data.te = data.sets[[i]]
+        data.tr = data[-data.te]
+        
+        fit = rpart(formula, data=data.tr, method="class")
+        
+    }
 }
 
 # data - a data fram
 # features - string vector of features to use
 # target -  
-crossValidate.linreg = function(data, features, target, folds=5) {
+crossValidate.logreg = function(data, features, target, folds=5) {
     n = nrow(data)
     formula = reformulate(features, target)
     
@@ -18,7 +30,9 @@ crossValidate.linreg = function(data, features, target, folds=5) {
     
     
     for (i in 1:5) {
-        data.tr = do.call("rbind", data.sets[[i]])
+        vector = 1:folds
+        vector = vector[-i]
+        data.tr = do.call("rbind", data.sets[[vector]])
         data.te = data.sets[[i]]
         
         fit = lm(formula, data=data.tr)
